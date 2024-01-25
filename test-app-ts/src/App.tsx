1669e67d4315b7d69907b04354f3749bdd3bab84
thao.vu <thaovu@miu.edu>
2024-01-25 13:34:49 -0600

    added test for typescript app

---
+++ test-app-ts/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+import { Container, Text } from "typetailui"
+function App() {
+  return (
+    <Container>
+      <Text>hello TypeTailUI</Text>
+    </Container>
+  );
+}
+
+export default App;
